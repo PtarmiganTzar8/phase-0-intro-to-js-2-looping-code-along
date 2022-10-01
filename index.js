5f769925d425f632e9c16ab7608ff6e2b190a124
Michael <michael.d.lange88@gmail.com>
2022-09-30 18:50:59 -0700

    commit tree 03cee3234623ed97723dab3f5f34b1f7f487ff58

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+
+function writeCards(names, event){
+    const thankyoucards = []
+    for (let i = 0; i < names.length; i++){
+        thankyoucards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankyoucards
+}
+
+function countDown(i){
+    while (i >= 0){
+        console.log(i)
+        i--
+    }
+}
+
+
+// console.log(writeCards(['George', 'Anne', 'Suzy'], 'birthday'))
+// console.log(writeCards(['June', 'Bob', 'Jack'], 'easter'))
+
+// thankyoucards.push()
+// invoking the function
